b049d67ebc6febcf2c30f075896cf6c82d0e2d36
Eugeniy <mycompyter@me.com>
2018-06-05 21:30:28 +0300

    Ilia privet

---
+++ app/src/main/java/com/zendoosbka/eugeniy/foruniver/MainActivity.java
@@ -0,0 +1,61 @@
+package com.zendoosbka.eugeniy.foruniver;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import com.google.android.gms.vision.*;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtView;
+    BarcodeDetector Detector;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void onClickButtonKik(View view)
+    {
+        dispatchTakePictureIntent();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtView = (TextView) findViewById(R.id.textView);
+
+        Detector = new BarcodeDetector.Builder(getApplicationContext())
+                .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                .build();
+        if(!Detector.isOperational()) {
+            txtView.setText("Could not set up the detector!");
+        }
+
+    }
+
+}
